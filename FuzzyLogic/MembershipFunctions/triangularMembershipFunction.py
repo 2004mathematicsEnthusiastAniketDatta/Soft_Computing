f1952726c7da2c7b5bca5aac5f4c3cb82941e413
2004_Aniket_Datta <116451512+2004mathematicsEnthusiastAniketDatta@users.noreply.github.com>
2025-04-24 13:46:41 +0000

    triangularmembershipfunction

--- FuzzyLogic/MembershipFunctions/triangularMembershipFunction.py
+++ FuzzyLogic/MembershipFunctions/triangularMembershipFunction.py
@@ -1,5 +1,5 @@
 import numpy as np 
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 
 a=float((input("Enter the left most point:")))
@@ -10,6 +10,23 @@ m=float(input("Enter the peak Value:"))
 x=(a,m,b)
 y=(0,1,0)
 for u,v in zip(x,y):
-    plt.text(u,v)
-    
+    plt.text(u,v,f"({u},{v})")
+plt.plot(x,y,marker='o')
+plt.title("Triangular Membership Function")
+plt.grid(False)
+plt.show()
 
+
+def triangularMembershipFunction(x):
+    if x <= a or x >= b:
+          mem=0
+    elif a < x <= m:
+          mem=(x-a)/(m-a)
+    elif m <= x < b:
+          mem= (b-x)/(b-m)
+    else: 
+            mem=0
+    print("Membership value is: ",mem)
+
+triangularMembershipFunction(4)
+    
